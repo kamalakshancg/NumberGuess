8fe176b9940d7577575fd4df53fd95479d365e22
kamalakshaNC <47631709+kamalakshancg@users.noreply.github.com>
2020-03-17 21:44:35 +0530

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -6,7 +6,8 @@ for i in range(0,5):
     while chance<5:
         user1=int(input("enter the your  number"))
         comp1=random.randint(0,2)
-        print("the computer number is",comp1)
+        print("the user number ->",user1)
+        print("the computer number ->",comp1)
         if(user1 == comp1 ):
             user+=1
         else:
